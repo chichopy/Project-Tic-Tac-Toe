b0a1f92824097090180fc2d0906e050572717a5b
Guillermo Sanchez <gsanz1997@gmail.com>
2022-09-25 19:06:11 -0400

    Create Matrix class
    In this class will be added all functions/methods related to the tic-tac-toe matrix

--- app.rb
+++ app.rb
@@ -1,8 +1,52 @@
 # frozen_string_literal: true
 
-$matrix = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+# This class mannages every function related to a tic-tac-toe matrix
+class Matrix
+  attr_accessor :matrix
 
-def user1_input
+  def initialize
+    @matrix = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+  end
+
+  def assign_value(position, value)
+    @matrix[position[0].to_i][position[1].to_i] = value
+  end
+
+  def check_three_in_line
+    flag1 = check_array(@matrix)
+    column_array = @matrix.transpose
+    flag2 = check_array(column_array)
+    dig_array = [[@matrix[0][0], @matrix[1][1], @matrix[2][2]],
+                 [@matrix[0][2], @matrix[1][1], @matrix[2][0]]]
+    flag3 = check_array(dig_array)
+    [flag1, flag2, flag3]
+  end
+
+  def check_array(array)
+    array.each do |arr|
+      next unless arr.uniq.size <= 1
+
+      puts arr.all?('O') ? 'User 1 wins' : 'User 2 wins'
+      return true
+    end
+  end
+end
+
+def user_choice
+  flag = true
+  while flag
+    puts 'Choose X or O'
+    choice1 = gets.chomp.upcase
+    next unless %w[X O].include?(choice1)
+
+    flag = false
+    choice2 = choice1 == 'X' ? 'O' : 'X'
+  end
+  [choice1, choice2]
+end
+
+def user_input(matrix)
+  array_2d_print(matrix)
   puts 'Enter x: '
   position_x = gets
   puts 'Enter y: '
@@ -10,27 +54,23 @@ def user1_input
   [position_x, position_y]
 end
 
-def user2_input
-  puts 'Enter x: '
-  position_x = gets
-  puts 'Enter y: '
-  position_y = gets
-  [position_x, position_y]
+def game(winner, choice)
+  matrix = Matrix.new
+  while winner == false
+    user1 = user_input(matrix.matrix)
+    winner = modify_matrix(user1, matrix, choice[0])
+    break if winner == true
+
+    user2 = user_input(matrix.matrix)
+    winner = modify_matrix(user2, matrix, choice[1])
+    array_2d_print(matrix.matrix)
+  end
 end
 
-def game
-  user1 = user1_input
-  modify_matrix(user1, 'O')
-  user2 = user2_input
-  modify_matrix(user2, 'X')
-end
-
-def modify_matrix(position, value)
-  $matrix[position[0].to_i][position[1].to_i] = value
-  # array_2d_print($matrix)
-  check_row($matrix)
-  check_column($matrix)
-  check_diagonals($matrix)
+def modify_matrix(position, matrix, value)
+  matrix.assign_value(position, value)
+  flags = matrix.check_three_in_line
+  flags.any?(true) ? true : false
 end
 
 def array_2d_print(array)
@@ -42,35 +82,4 @@ def array_2d_print(array)
   end
 end
 
-def check_row(array)
-  array.each do |arr|
-    next unless arr.uniq.size <= 1
-
-    puts arr.all?('O') ? 'User 1 wins' : 'User 2 wins'
-  end
-end
-
-def check_column(array)
-  column_array = array.transpose
-  # array_2d_print($matrix)
-  column_array.each do |arr|
-    next unless arr.uniq.size <= 1
-
-    puts arr.all?('O') ? 'User 1 wins' : 'User 2 wins'
-  end
-end
-
-def check_diagonals(array)
-  dig_array = [[array[0][0], array[1][1], array[2][2]],
-               [array[0][2], array[1][1], array[2][0]]]
-
-  dig_array.each do |arr|
-    next unless arr.uniq.size <= 1
-
-    puts arr.all?('O') ? 'User 1 wins' : 'User 2 wins'
-  end
-end
-
-3.times do
-  game
-end
+game(false, user_choice)
